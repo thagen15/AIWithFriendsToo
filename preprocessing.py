7a62da016fa1137b4652d7eb336e501a1575703c
thagen15 <thagen@wpi.edu>
2018-02-21 16:07:04 -0500

    need to fill in model

--- preprocessing.py
+++ preprocessing.py
@@ -9,57 +9,38 @@ def showImage(img):
     plt.gray()
     plt.imshow(image)
     plt.show()
-# def returnVectors(imagePath, labelPath):
-def printImage(image):
-    count=0
-    thingToPrint = ""
-    for bits in image:
-        count+=1
-        thingToPrint += str(bits)+" "
-        if count%28==0:
-            print thingToPrint
-            thingToPrint =""
 
+def process(imagePath, labelsPath):
+    images = np.load(imagePath)
+    image_vectors = []
 
-images = np.load("images.npy")
-#print images[4]
-image_vectors = []
-# #Flatten each 28x28 matrix
-# for image in images:
-#     image_vector = np.reshape(image,-1)
-#     image_vectors.append(image_vector)
+    image_vectors = images.reshape(6500, 784)
+    image_vectors = np.array(image_vectors)
+    print image_vectors[0].shape
 
-image_vectors = images.reshape(6500, 784)
-image_vectors = np.array(image_vectors)
-print image_vectors[0].shape
-#print image_vectors.shape
-#labels = np.load("labels.npy")
+    labels = np.load(labelsPath)
+    labels_flat = labels.reshape(6500, 1)
+    label_vectors = []
+    one_hot_labels = keras.utils.to_categorical(labels_flat, num_classes=10)
+    label_vectors = np.array(one_hot_labels)
+    print label_vectors[1].shape
 
-labels = np.load("labels.npy")
-labels_flat = labels.reshape(6500, 1)
-label_vectors = []
-one_hot_labels = keras.utils.to_categorical(labels_flat, num_classes=10)
-# #make one hot encoding with labels
-# for label in labels:
-#     label_vector = keras.utils.to_categorical(label, num_classes = 10)
-#     label_vectors.append(label_vector)
-label_vectors = np.array(one_hot_labels)
-print label_vectors[1].shape
-#print label_vectors.shape
-#print label_vectors[0]
+    data = np.column_stack((label_vectors, image_vectors))
+    np.random.shuffle(data)
 
-data = np.column_stack((label_vectors, image_vectors))
-np.random.shuffle(data)
+    one_hot_labels = data[:,0:10]
+    flattend_images = data[:,10:]
+    print one_hot_labels[1]
+    print ""
+    showImage(flattend_images[1])
 
-one_hot_labels = data[:,0:10]
-flattend_images = data[:,10:]
-#one_hot_labels, flattend_images = np.split(data,2,10)
-print one_hot_labels[1]
-print ""
-showImage(flattend_images[1])
-#print data[1]
+    data_size = 6500
+    training_size = int(data_size * 0.60)
+    print ("Training Set size = ", training_size)
+    val_size = int(data_size * (0.65 + 0.15))
+    print ("Validation set index = ", val_size)
 
-#     return data
-#
-# if __name__== "__main__":
-#     returnVectors()
+    num_classes = np.unique(labels).shape[0] #which is 10
+    x_train, x_val, x_test = flattend_images[:training_size], flattend_images[training_size:val_size], flattend_images[val_size:]
+    y_train, y_val, y_test = one_hot_labels[:training_size], one_hot_labels[training_size: val_size], one_hot_labels[val_size:]
+    return x_train, y_train, x_val, y_val, x_test, y_test
--- template.py
+++ template.py
@@ -1,8 +1,10 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 import numpy as np
+import preprocessing
 
 # Model Template
+x_train, y_train, x_val, y_val, x_test, y_test = preprocessing.process("images.npy","labels.npy")
 
 model = Sequential() # declare model
 model.add(Dense(10, input_shape=(28*28, ), kernel_initializer='he_normal')) # first layer
@@ -13,23 +15,24 @@ model.add(Activation('relu'))
 # Fill in Model Here
 #
 #
+
 model.add(Dense(10, kernel_initializer='he_normal')) # last layer
 model.add(Activation('softmax'))
 
 
 # Compile Model
 model.compile(optimizer='sgd',
-              loss='categorical_crossentropy', 
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 # Train Model
-history = model.fit(x_train, y_train, 
-                    validation_data = (x_val, y_val), 
-                    epochs=10, 
+history = model.fit(x_train, y_train,
+                    validation_data = (x_val, y_val),
+                    epochs=10,
                     batch_size=512)
 
 
 # Report Results
 
 print(history.history)
-model.predict()
+# model.predict(x_test,y_test)
