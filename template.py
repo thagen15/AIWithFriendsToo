1e37ec91ca5e5624a7355934f9d431c9740040e2
ppatias <ppatias@wpi.edu>
2018-02-23 22:35:08 -0500

    added metric in template

--- template.py
+++ template.py
@@ -4,6 +4,7 @@ import numpy as np
 import preprocessing
 from sklearn.metrics import precision_score, recall_score, f1_score
 from sklearn.metrics import confusion_matrix
+from sklearn import metrics
 
 # Model Template
 x_train, y_train, x_val, y_val, x_test, y_test = preprocessing.process("images.npy","labels.npy")
@@ -13,24 +14,13 @@ model = Sequential() # declare model
 model.add(Dense(10, input_shape=(28*28, ), kernel_initializer='he_normal')) # first layer
 model.add(Activation('relu'))
 
-
-
 #
 model.add(Dense(10, kernel_initializer='he_normal')) # last layer
 model.add(Activation('tanh'))
 
-model.add(Dense(10, kernel_initializer='he_normal')) # last layer
-model.add(Activation('tanh'))
-
-model.add(Dense(10, kernel_initializer='he_normal')) # last layer
-model.add(Activation('tanh'))
 #
-
-
-
 model.add(Dense(10, kernel_initializer='he_normal')) # last layer
 model.add(Activation('softmax'))
-
 
 # Compile Model
 model.compile(optimizer='sgd',
@@ -40,21 +30,26 @@ model.compile(optimizer='sgd',
 # Train Model
 history = model.fit(x_train, y_train,
                     validation_data = (x_val, y_val),
-                    epochs=40,
+                    epochs=10,
                     batch_size=512)
 
 
 # Report Results
 
 print(history.history)
+expected = y_test
 predict_arr = model.predict(x_test)
 #print (predict_arr.shape)
 y_act = list(map(np.argmax, y_test))
 y_pred = list(map(np.argmax, predict_arr))
-print(y_act)
-print(y_pred)
+#print(y_act)
+#print(y_pred)
+
 print("Confusion Matrix")
 print(confusion_matrix(y_act,y_pred, [0,1,2,3,4,5,6,7,8,9]))
+
+print ("metrics")
+print(metrics.classification_report(y_act, y_pred))
 
 #print("Evaluating performance...")
 #precision = precision_score(y_test, predict_arr, average=None)	# Calculate the precision
